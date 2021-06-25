4197931268e892b962a329c9daff94228451d427
Kamila Galiaskarova <71707087+vinograd42@users.noreply.github.com>
2021-06-25 15:59:56 +0300

    у

--- javascripts/menu.js
+++ javascripts/menu.js
@@ -1,9 +1,23 @@
 $(document).ready(function() {
-$('.menu').click(function(){
-$('.menu_mini').css("display","block");
-$('.mainmenu').removeClass('none');
-});
-$('.icon_close').click(function(){
-$('.mainmenu').addClass('none');
-});
-});
+  $('.menu').click(function(){
+    $('.menu_mini').css("display","block");
+      $('.mainmenu').removeClass('none');
+    });
+    $('.icon_close').click(function(){
+      $('.mainmenu').addClass('none');
+    });
+  });
+
+  $(document).ready(function() {
+    $('button').click(function(){
+      $('button').addClass('color');
+        $('button').html('ПОСТАВЛЕНО');
+      });
+    });
+
+    $(document).ready(function() {
+      $('button').click(function(){
+        $('button').addClass('color');
+        $('button').html('ЗАБРОНИРОВАНО');
+      });
+    });
